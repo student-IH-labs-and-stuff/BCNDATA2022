a8dcc580b9c78c18bf84f68c420266bf73aad5f0
siandav <sian.davies@ironhack.com>
2022-04-26 17:19:02 +0200

    Update day1.sql

--- ClassMaterials/Week3/sians sql/day1.sql
+++ ClassMaterials/Week3/sians sql/day1.sql
@@ -70,11 +70,64 @@ SELECT max(amount), min(amount) FROM loan;
 # average order amount 
 SELECT round(avg(amount),3), ceiling(avg(amount)),
  floor(avg(amount)) from bank.order; 
+ 
+ # useful string functions - LOWER UPPER LENGTH CONCAT LEFT RIGHT LTRIM RTRIM
+ SELECT A2, LEFT(A2,5) FROM district;
+ 
+ SELECT LTRIM('   Praha'); #trim all white space from left 
+ 
+ #Select districts and salaries (from the district table) 
+ #where salary is greater than 10000. 
+ #Return columns as district_name and average_salary.
+ 
+ SELECT A2 AS district_name, A11 as avg_salary
+ FROM district 
+ WHERE A11 > 10000;
 
+ 
+ #Select those loans whose contract is finished and not paid back.
+ #Return the debt value from the status you identified in the last activity,
+ #together with loan id and account id.
+ SELECT loan_id, account_id, amount-payments as debt 
+ FROM loan where status= 'B';
+#Calculate the urban population for all districts. 
+#Hint: You are looking for the number of inhabitants 
+#and the % of urban inhabitants in each district. 
+#Return columns as district_name and urban_population.
 
+SELECT ROUND((A4*A10)/100) AS urbanpop, A2 as districtname from district
+where A10 <= 50;
 
+#On the previous query result - 
+#rerun it but filter out districts where the rural 
+#population is greater than 50%.
 
+# dates CONVERT(int,date), SUBSTRING_INDEX(), DATE_FORMAT(%Y)
+SELECT *, convert(account.date,date) as mydate FROM bank.account;
+SELECT *, convert(substring_index(card.issued,' ',1),date) as newdate 
+FROM bank.card;
+SELECT *, 
+date_format(convert(loan.date,date),'%Y-%m-%d') as newdate,
+date_format(convert(loan.date,date),'%Y-%m') as yearmonth
+FROM loan;
 
+# nulls function isnull() and where is not null() or where is null()
+SELECT sum(isnull(loan_id)) from loan;
+SELECT * FROM loan where loan_id is null;
 
+# case - we have done in tableau - loan table - replace ABCD with definitions
+#structure is :
+#CASE 
+#WHEN 
+#THEN 
+#ELSE 
+#END 
 
-
+SELECT *,
+CASE status
+WHEN 'A' THEN 'GOOD-PAID OFF'
+WHEN 'B' THEN 'ENDED-Not paid'
+WHEN 'C' THEN 'GOOD-RUNNING'
+ELSE 'RUNNING-missed payment'
+END AS description
+from loan;
